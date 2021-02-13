9a7b2f2099438aea4043583188df4a765c87af5a
Thiago Seidel <thiagosseidel@gmail.com>
2021-02-12 22:06:16 -0300

    Piece move count #6 - #9

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -112,7 +112,10 @@ public boolean getCheckMate() {
 	
 	private Piece makeMove(Position source, Position target) {
 		
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		
+		p.increaseMoveCount();
+
 		Piece capturedPiece = board.removePiece(target);
 		
 		board.placePiece(p, target);
@@ -128,7 +131,9 @@ public boolean getCheckMate() {
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
 		
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece) board.removePiece(target);
+		
+		p.decreaseMoveCount();
 		
 		board.placePiece(p, source);
 		
