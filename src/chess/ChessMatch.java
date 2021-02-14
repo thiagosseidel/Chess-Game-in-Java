4bbe587f04168cefd90bf743c3641c76fbd0e20f
Thiago Seidel <thiagosseidel@gmail.com>
2021-02-14 15:53:08 -0300

    Better treatment to promotion #6 - #7 - #20

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -162,7 +161,7 @@ public class ChessMatch {
 		
 		if (!type.equals("Q") && !type.equals("B") && !type.equals("N") && !type.equals("R")) {
 			
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
