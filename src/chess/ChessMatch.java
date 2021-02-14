06f329ef3f5080cd4b8aabac7650326e5cbbcc78
Thiago Seidel <thiagosseidel@gmail.com>
2021-02-14 12:47:14 -0300

    Special move - Castling #6 - #11

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -129,6 +129,30 @@ public boolean getCheckMate() {
 			capturedPieces.add(capturedPiece);
 			piecesOnTheBoard.remove(capturedPiece);
 		}
+		
+		// #specialmove
+		if (p instanceof King) {
+			
+			// castling kingside rook
+			if (target.getColumn() == (source.getColumn() + 2)) {
+				
+				Position sourceRook = new Position(source.getRow(), source.getColumn() + 3);
+				Position targetRook = new Position(source.getRow(), source.getColumn() + 1);
+				
+				makeMove(sourceRook, targetRook);
+				
+			}
+			
+			// castling queenside rook
+			if (target.getColumn() == (source.getColumn() - 2)) {
+				
+				Position sourceRook = new Position(source.getRow(), source.getColumn() - 4);
+				Position targetRook = new Position(source.getRow(), source.getColumn() - 1);
+				
+				makeMove(sourceRook, targetRook);
+				
+			}
+		}
 
 		return capturedPiece;
 	}
@@ -147,6 +171,30 @@ public boolean getCheckMate() {
 			
 			piecesOnTheBoard.add(capturedPiece);
 			capturedPieces.remove(capturedPiece);
+		}
+		
+		// #specialmove
+		if (p instanceof King) {
+			
+			// castling kingside rook
+			if (target.getColumn() == (source.getColumn() + 2)) {
+				
+				Position sourceRook = new Position(source.getRow(), source.getColumn() + 3);
+				Position targetRook = new Position(source.getRow(), source.getColumn() + 1);
+				
+				undoMove(sourceRook, targetRook, null);
+				
+			}
+			
+			// castling queenside rook
+			if (target.getColumn() == (source.getColumn() - 2)) {
+				
+				Position sourceRook = new Position(source.getRow(), source.getColumn() - 4);
+				Position targetRook = new Position(source.getRow(), source.getColumn() - 1);
+				
+				undoMove(sourceRook, targetRook, null);
+				
+			}
 		}
 	}
 
@@ -282,12 +330,12 @@ public boolean getCheckMate() {
 		
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('b', 1, new Bishop(board, Color.WHITE));
-        placeNewPiece('g', 1, new Bishop(board, Color.WHITE));
-        placeNewPiece('c', 1, new Knight(board, Color.WHITE));
-        placeNewPiece('f', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('b', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE, this));
 
         
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
@@ -301,11 +349,11 @@ public boolean getCheckMate() {
         
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('b', 8, new Bishop(board, Color.BLACK));
-        placeNewPiece('g', 8, new Bishop(board, Color.BLACK));
-        placeNewPiece('c', 8, new Knight(board, Color.BLACK));
-        placeNewPiece('f', 8, new Knight(board, Color.BLACK));
+        placeNewPiece('b', 8, new Knight(board, Color.BLACK));
+        placeNewPiece('g', 8, new Knight(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK, this));
 	}
 }
